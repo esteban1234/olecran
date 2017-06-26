58406b335f2310527c812b1e232c56a971a63db6
esteban1234 <tva_libra@hotmail.com>
2017-06-26 17:44:10 -0500

    pagina lista

--- vistas/contacto.php
+++ vistas/contacto.php
@@ -1,7 +1,133 @@
 <!DOCTYPE html>
 <?php include 'encabezado2.php' ?>
 <body>
+	<div class="contienetodo">
+		<div class="header">
+	        <nav class="pull-left">
+	            <ul>
+	                <a href="../index.php">INICIO</a>
+	                <a href="nosotros.php">NOSOTROS</a>
+	                <a href="servicios.php">SERVICIOS</a>
+	                <a class="select" href="contacto.php">CONTACTO</a>
+	            </ul>
+	        </nav>
+	        <figure class="logo pull-right">
+	            <img src="../img/olecran.png" alt="OLECRAM" class="img-responsive">
+	        </figure>
+	    </div>
 
+	    <div class="contenformmap">
+	    	<div class="container formu pull-left">
+				<div class="row">
+				    <div class="col-md-12">
+						<div class="form_main">
+				            <h4 class="heading"><strong>OLECRAM</strong> Contacto<span></span></h4>
+				            <div class="form">
+				                <form action="contact_send_mail.php" method="post" id="contactFrm" name="contactFrm">
+				                    <input type="text" required="" placeholder="Escribe tu nombre completo" value="" name="name" class="txt">
+				                    <input type="text" required="" placeholder="Escribe tu numero telefonico completo" value="" name="mob" class="txt">
+				                    <input type="text" required="" placeholder="Escribe tu correo" value="" name="email" class="txt">
+				                    
+				                	 <textarea placeholder="Escribe tu comentario" name="mess" type="text" class="txt_3"></textarea>
+				                     <input type="submit" value="Enviar" name="submit" class="txt2">
+				                </form>
+				            </div>
+				        </div>
+				    </div>
+				</div>
+			</div>
+
+			<div class="datos pull-right">
+			    <div class="container">
+				    <div class="row">
+				    	<div class="col-md-3">
+							<div class="offer offer-default">
+								<div class="shape">
+									<div class="shape-text">
+										<span class=""><i class="fa fa-street-view" aria-hidden="true"></i></span>							
+									</div>
+								</div>
+								<div class="offer-content">
+									<h3 class="lead">
+									Dirección:
+									</h3>
+									<p>
+										 Av. Paseo de los Leones #1684 por C. 20VA Av. y Paseo de los Insurgentes. Col. Las Cumbres 1 Sector, Monterrey, N.L. C.P. 64610 
+										<br> 
+				                       
+									</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-3">
+							<div class="offer offer-default">
+								<div class="shape">
+									<div class="shape-text">
+										<span class=""><i class="fa fa-phone" aria-hidden="true"></i></span>							
+									</div>
+								</div>
+								<div class="offer-content">
+									<h3 class="lead">
+										 Teléfono:
+									</h3>
+									<p>
+										PENDIENTE
+										<br> 
+				                       
+									</p>
+								</div>
+							</div>
+						</div>
+				    </div>
+
+                    <div class="row">
+				    	<div class="col-md-3">
+							<div class="offer offer-default">
+								<div class="shape">
+									<div class="shape-text">
+										<span class=""><i class="fa fa-envelope-o" aria-hidden="true"></i></span>							
+									</div>
+								</div>
+								<div class="offer-content">
+									<h3 class="lead">
+									Correo:
+									</h3>
+									<p>
+										 info@olecram.org
+										<br> 
+				                       
+									</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-3">
+							<div class="offer offer-default">
+								<div class="shape">
+									<div class="shape-text">
+										<span class=""><i class="fa fa-clock-o" aria-hidden="true"></i></span>							
+									</div>
+								</div>
+								<div class="offer-content">
+									<h3 class="lead">
+										 Horario:
+									</h3>
+									<p>
+										Lunes a Viernes de 9am a 6pm
+										<br> 
+				                       
+									</p>
+								</div>
+							</div>
+						</div>
+        			</div>
+				</div>	
+			</div>
+	    </div>
+
+	    <div class="mapa">
+	    	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3595.1087032867467!2d-100.36804728075263!3d25.700824690662696!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8662966e9e0525c3%3A0xb5bf064757593be!2sAv+Paseo+de+los+Leones+1684%2C+Leones%2C+64610+Monterrey%2C+N.L.!5e0!3m2!1ses-419!2smx!4v1498516711236" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+	    </div>
+	</div>    
 
 
 <span class="ir-arriba"><i class="fa fa-chevron-up" aria-hidden="true"></i></span>
--- vistas/servicios.php
+++ vistas/servicios.php
@@ -2,6 +2,83 @@
 <?php include 'encabezado2.php' ?>
 <body>
 
+	<div class="contienetodo">
+		<div class="header">
+	        <nav class="pull-left">
+	            <ul>
+	                <a href="../index.php">INICIO</a>
+	                <a href="nosotros.php">NOSOTROS</a>
+	                <a class="select" href="servicios.php">SERVICIOS</a>
+	                <a href="contacto.php">CONTACTO</a>
+	            </ul>
+	        </nav>
+	        <figure class="logo pull-right">
+	            <img src="../img/olecran.png" alt="OLECRAM" class="img-responsive">
+	        </figure>
+	    </div>
+
+	    <div class="servicios">
+
+	    <p>EL SERVICIO INTEGRAL OLECRAM S.C.P.</p>
+	    	
+	    </div>
+
+	    <div class="contenser">
+	    	<div class="servuno">
+	    		<div class="servunoo">
+	    			<span><i class="fa fa-calculator" aria-hidden="true"></i></span>
+	    			<p class="tit">Consultoria en administración</p>
+	    			<p class="subtit">Guiamos a las empresas en su evolución y crecimiento, facilitando una migración suave y eficaz desde un modelo primario a otro fundamentado en una organización formalizada.</p>
+	    		</div>
+	    	</div>
+
+	    	<div class="servuno">
+	    		<div class="servunoo">
+	    			<span><i class="fa fa-handshake-o" aria-hidden="true"></i></span>
+	    			<p class="tit">Asesoria empresarial</p>
+	    			<p class="subtit">Utilizamos nuestra tecnología, conocimientos y experiencia en materia financiera y analítica, para apoyar a empresas a desarrollar e implementar sus estrategias.</p>
+	    		</div>
+	    	</div>
+
+	    	<div class="servuno">
+	    		<div class="servunoo">
+	    			<span><i class="fa fa-usd" aria-hidden="true"></i></span>
+	    			<p class="tit">Asesoria financiera</p>
+	    			<p class="subtit">La Asesoria financiera para las empresas desde la más pequeña hasta la gran corporación inclusive requiere de servicios que hacen particular énfasis en cómo las empresas pueden crear valor y mantenerlo a través del uso eficiente de los recursos financieros.</p>
+	    		</div>
+
+	    	</div>
+
+	    	<div class="servuno">
+	    		<div class="servunoo">
+	    			<span><i class="fa fa-gavel" aria-hidden="true"></i></span>
+	    			<p class="tit">Trámites legales</p>
+	    			<p class="subtit">Debido a la complejidad de la legislación vigente resulta indispensable contar con un constante acompañamiento y un correcto conocimiento de las normativas. Contamos con las herramientas adecuadas para dar la respuesta más acertada a las necesidades legales.</p>
+	    		</div>
+	    	</div>
+
+	    	<div class="servuno">
+	    		<div class="servunoo">
+	    			<span><i class="fa fa-user-secret" aria-hidden="true"></i></span>
+	    			<p class="tit">Supervisión de Construcción</p>
+	    			<p class="subtit">Contamos con una metodología para vigilar la coordinación de actividades con el objetivo de cumplir a tiempo las condiciones técnicas y económicas estipuladas en el contrato de obra.</p>
+	    		</div>
+	    	</div>
+
+	    	<div class="servuno">
+	    		<div class="servunoo">
+	    			<span><i class="fa fa-building" aria-hidden="true"></i></span>
+	    			<p class="tit">Inspección de edificios</p>
+	    			<p class="subtit">La finalidad es permitir conocer el estado de los edificios para garantizar su buen estado y seguridad. Olecram revelará los deterioros con el objetivo de elaborar un plan de intervención y un plan de mantenimiento que permitan ampliar la vida útil del edificio.</p>
+	    		</div>
+	    	</div>
+	    </div>
+
+	    <div class="contacto">
+	    	<a href="contacto.php"><p>AGENDA UNA CITA</p></a>
+	    </div>
+	</div>
+
 
 <span class="ir-arriba"><i class="fa fa-chevron-up" aria-hidden="true"></i></span>
 <?php include 'pie.php' ?>
