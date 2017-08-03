5dd59089608f9017c67eaa6d2ae92463bc6afc8b
champiiy <albert1047@hotmail.com>
2017-08-03 11:12:23 -0500

    UPDATE RESPONSIVO V1.2

--- index.php
+++ index.php
@@ -35,41 +35,40 @@
        <div class="section seccion1">
       <div class="container">
         <div class="row">
-          <div class="col-md-12">
+          <div class="col-xs-12 col-sm-12 col-md-12">
             <h1 class="text-center tit-index" style="color: #fff; margin-top: 5%;">Algunos servicios</h1>
             <p class="lead text-center" style="color: #fff;">Te ofrecemos servicios con altos estándares de calidad.</p>
           </div>
         </div>
-        <div class="row">
-          <div class="col-md-2">
-            <img src="img/asesoria-en-administracion.jpg" class="img-responsive img-thumbnail">
+        <div class="row img-width">
+          <div class="col-xs-12 col-sm-2 col-md-2">
+            <img src="img/asesoria-en-administracion.jpg" class="img-responsive img-thumbnail img-width">
           </div>
-          <div class="col-md-4">
+          <div class="col-xs-12 col-sm-4 col-md-4">
             <h3 class="text-left subtit-index" style="color: #fff; margin-top: 17%;">Consultoria en administración</h3>
             <!-- <p class="text-left" style="color: #fff;">Lorem ipsum dolor sit amet, adipiscing elit Aenean commodo ligula eget.</p> -->
           </div>
-          <div class="col-md-2">
+          <div class="col-xs-12 col-sm-2 col-md-2">
             <img src="img/legal2.jpg" class="img-responsive img-thumbnail">
           </div>
-          <div class="col-md-4">
+          <div class="col-xs-12 col-sm-4 col-md-4">
             <h3 class="text-left subtit-index" style="color: #fff; margin-top: 17%;">Apoyo para efectuar trámites legales</h3>
             <!-- <p class="text-left" style="color: #fff;">Lorem ipsum dolor sit amet, adipiscing elit Aenean commodo ligula eget.</p> -->
           </div>
         </div>
         <br>
         <div class="row">
-          <div class="col-md-2">
+          <div class="col-xs-12 col-sm-2 col-md-2">
             <img src="img/asesoria-financiera.jpg" class="img-responsive img-thumbnail">
           </div>
-          <div class="col-md-4">
+          <div class="col-xs-12 col-sm-4 col-md-4">
             <h3 class="text-left subtit-index" style="color: #fff; margin-top: 17%;">Asesoria financiera</h3>
             <!-- <p class="text-left" style="color: #fff;">Lorem ipsum dolor sit amet, adipiscing elit Aenean commodo ligula eget.</p> -->
           </div>
-          <div class="col-md-2">
-            <img src="img/imagen2.jpg"
-            class="img-responsive img-thumbnail">
+          <div class="col-xs-12 col-sm-2 col-md-2">
+            <img src="img/imagen2.jpg" class="img-responsive img-thumbnail">
           </div>
-          <div class="col-md-4 text-center">
+          <div class="col-xs-12 col-sm-4 col-md-4 text-center">
             <h3 class="text-left subtit-index" style="color: #fff; margin-top: 17%;">Asesoria empresarial</h3>
             <!-- <p class="text-left" style="color: #fff;">Lorem ipsum dolor sit amet, adipiscing elit Aenean commodo ligula eget.</p> -->
           </div>
